3d47324103158053eb4b6e0a6c25a79744333356
Nicolas Guignard <nicolas065@gmail.com>
2022-07-06 10:49:27 +0200

    reset

--- kata1/src/main/scala/TicTacToe.scala
+++ kata1/src/main/scala/TicTacToe.scala
@@ -1,9 +1,1 @@
-case class TicTacToe(grid: List[String]) {
-  def getAt(position: String): Option[Player] = grid.find(e => e == position)
-
-  def play(position: String): TicTacToe = copy(grid=position :: grid)
-}
-
-sealed trait Player
-case object Cross extends Player
-case object Circle extends Player
+case class TicTacToe()
--- kata1/src/test/scala/TicTacToeTest.scala
+++ kata1/src/test/scala/TicTacToeTest.scala
@@ -3,18 +3,7 @@ import flatspec._
 import matchers._
 
 class TicTacToeTest extends AnyFlatSpec with should.Matchers {
+  "Tictactoe" should "be able to DO SOMETHING" in {
 
-  "Tictactoe" should "be able to insert at a1" in {
-    val tictactoe = TicTacToe()
-
-    val newTictactoe = tictactoe.play("a1")
-    newTictactoe.getAt("a1") should be(Some(Cross))
-  }
-
-  it should "be able to insert at a1 and a2" in {
-    val tictactoe = TicTacToe()
-    val newTictactoe = tictactoe.play("a1").play("a2")
-    newTictactoe.getAt("a1") should be (Some(Cross))
-    newTictactoe.getAt("a2") should be (Some(Circle))
   }
 }
