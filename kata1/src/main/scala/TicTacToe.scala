e7072710565cae43572f72a77c577e70bcfc07bd
Grégoire Chauvet <gchauvet.home@gmail.com>
2022-07-06 09:29:49 +0000

    pouf

--- kata1/src/main/scala/TicTacToe.scala
+++ kata1/src/main/scala/TicTacToe.scala
@@ -1,4 +1,12 @@
-case class TicTacToe() {
-  def getGrid(): List[Nothing] = Nil
-
+case class TicTacToe(
+  a1: Option[Nothing] = None,
+  a2: Option[Nothing] = None,
+  a3: Option[Nothing] = None,
+  b1: Option[Nothing] = None,
+  b2: Option[Nothing] = None,
+  b3: Option[Nothing] = None,
+  c1: Option[Nothing] = None,
+  c2: Option[Nothing] = None,
+  c3: Option[Nothing] = None
+) {
 }
--- kata1/src/test/scala/TicTacToeTest.scala
+++ kata1/src/test/scala/TicTacToeTest.scala
@@ -8,11 +8,21 @@ class TicTacToeTest extends AnyFlatSpec with should.Matchers {
   "TicTacToe" should "be created with a blank grid" in {
     val ticTacToe = TicTacToe()
 
-    ticTacToe.getGrid() should be (Nil)
+    ticTacToe.a1 should be (None)
+    ticTacToe.a2 should be (None)
+    ticTacToe.a3 should be (None)
+    ticTacToe.b1 should be (None)
+    ticTacToe.b2 should be (None)
+    ticTacToe.b3 should be (None)
+    ticTacToe.c1 should be (None)
+    ticTacToe.c2 should be (None)
+    ticTacToe.c3 should be (None)
   }
+
   "TicTacToe" should "BLABLA" in {
     val ticTacToe = TicTacToe()
 
+    ticTacToe.play("a1")
 //    ticTacToe.getGrid() should be (Nil)
   }
 }
