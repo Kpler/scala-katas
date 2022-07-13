165927097873b355fa501cab2bde0462947e9dfa
danchemla <36636437+danchemla@users.noreply.github.com>
2022-07-13 09:33:20 +0000

    add stuff for test

--- TicTacToe/src/test/scala/Example.scala
+++ TicTacToe/src/test/scala/Example.scala
@@ -8,4 +8,11 @@ class Example extends AnyFlatSpec with should.Matchers {
     val stack = List(3, 2)
     stack.drop(1) should be (List(2))
   }
+
+  "A List length" should "be of size 9" in {
+    val valid_matrix = List.fill(9)(False)
+    valid_matrix.length() should be 9
+  }
+
+
 }
