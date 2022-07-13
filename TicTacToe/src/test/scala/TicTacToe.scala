ff0646e766ecfa68b73b7e330f95157abe2a6cd6
Guillaume MOCQUET <1082289+gmocquet@users.noreply.github.com>
2022-07-13 09:43:41 +0000

    guillaume

--- TicTacToe/src/test/scala/TicTacToe.scala
+++ TicTacToe/src/test/scala/TicTacToe.scala
@@ -4,11 +4,20 @@ import matchers._
 
 class TestTicTacToe extends AnyFlatSpec with should.Matchers {
 
+  "The app" should " for users input" in {
+    // When
+    val newGame = new TicTacToe()
+    
+    // Then
+    // check the grid is empty
+    newGame.grid should be List(List(), List(), List())
+  }
+
   "The app" should "asks for users input" in {
     // Given
     val newGame = new TicTacToe()
 
     // When
-    Main.main()
+     
   }
 }
