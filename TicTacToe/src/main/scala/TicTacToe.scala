1b48b426997db833c0152918c4c6c18d48d478f4
Nicolas Guignard <nicolas065@gmail.com>
2022-07-13 12:07:31 +0200

    WIP

--- TicTacToe/src/main/scala/TicTacToe.scala
+++ TicTacToe/src/main/scala/TicTacToe.scala
@@ -1,6 +1,16 @@
+import TicTacToe.AUTHORIZED_POSITIONS
 
+object TicTacToe {
+  private val AUTHORIZED_POSITIONS = Set("a1", "a2", "a3", "b1", "b2", "b3", "c1", "c2", "c3")
+}
 case class TicTacToe(val grid: Set[String] = Set()) {
-  def play(position: String): TicTacToe = {
-    TicTacToe(grid ++ Set( position))
+
+  def play(position: String): Either[String, TicTacToe] = {
+    if (grid.contains(position))
+      Left("Already contained position")
+    else if (!AUTHORIZED_POSITIONS.contains(position))
+      Left("Invalid position")
+    else
+      Right(TicTacToe(grid ++ Set(position)))
   }
 }
--- TicTacToe/src/test/scala/TicTacToeTest.scala
+++ TicTacToe/src/test/scala/TicTacToeTest.scala
@@ -3,6 +3,7 @@ import flatspec._
 import matchers._
 
 import scala.collection.mutable
+import scala.util.Failure
 
 class TicTacToeTest  extends AnyFlatSpec with should.Matchers {
   "A Tictactoe" should "initialise with the empty grid" in {
@@ -15,25 +16,24 @@ class TicTacToeTest  extends AnyFlatSpec with should.Matchers {
     val game = TicTacToe()
     val result = game.play("b3")
 
-    result.grid should have size 1
-    result.grid should contain ("b3")
+    result.right.get.grid should have size 1
+    result.right.get.grid should contain ("b3")
   }
 
   "A Player" should "not be able to take an occupied cell" in {
     val game = TicTacToe()
-    val result = game.play("b3").play("b3")
+    val result = game.play("b3").right.get.play("b3")
 
-    result.grid should have size 1
+    result should be (Left("Already contained position"))
   }
 
-  "A Player" should "be only able to play in a grid of 3x3 cells " in{ 
+  "A Player" should "be only able to play in a grid of 3x3 cells " in{
      val game = TicTacToe()
 
      val result = game.play("b4")
-    
-    result.grid should have size 0
 
-
+    result should be (Left("Invalid position"))
   }
+
 }
 
