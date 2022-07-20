081d2e78fb6c142cc41258b7b928830b6a6dd3ea
Henry Nomichith <hnomichith@kpler.com>
2022-07-20 11:46:43 +0200

    henry

--- TicTacToe/src/test/scala/Example.scala
+++ TicTacToe/src/test/scala/Example.scala
@@ -21,22 +21,23 @@ class Example extends AnyFlatSpec with should.Matchers {
     test.left.get should be ("invalid move")
   }
 
+  "to add a already done move" should "be an invalid move" in {
+    val game = List((1,1))
+
+    val test = Manager.move(game, (42, 42))
+
+    test.left.get should be ("invalid move")
+  }
+
 
 }
 
 
 object Manager {
   def move(list: List[(Int, Int)], move: (Int, Int)): Either[String, List[(Int, Int)]] = {
-    move match:
-      case (0, 0): Right(list :+ move)
-      case (0, 1): Right(list :+ move)
-      case (0, 2): Right(list :+ move)
-      case (1, 0): Right(list :+ move)
-      case (1, 1): Right(list :+ move)
-      case (1, 2): Right(list :+ move)
-      case (2, 0): Right(list :+ move)
-      case (2, 1): Right(list :+ move)
-      case (2, 2): Right(list :+ move)
-      case _: Left("invalid move")
+    move match {
+      case (x , y) if x <3 && y < 3 => Right(list :+ move)
+      case _ => Left("invalid move")
+    }
   }
 }
