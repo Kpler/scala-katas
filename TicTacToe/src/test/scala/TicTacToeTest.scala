b76e9e2c6b8b565ce0994e9d2063c20d49a443bf
Ahmed Ftouh <ftouhahmed@gmail.com>
2022-07-20 11:42:38 +0200

    WIP

--- TicTacToe/src/test/scala/TicTacToeTest.scala
+++ TicTacToe/src/test/scala/TicTacToeTest.scala
@@ -2,24 +2,23 @@ import org.scalatest._
 import flatspec._
 import matchers._
 
-case class TicTacToe(val grid: List[String] = Nil) {
-  // grid = List(9, Nil)
+case class TicTacToe(val grid: List[String] = List(null, null, null, null, null, null, null, null, null) ) {
+
 }
 
 
 class TicTacToeTest extends AnyFlatSpec with should.Matchers {
-  "TicTacToe" should "be instantiated with an empty grid" {
+  "TicTacToe" should "be instantiated with an 9 empty elements" in {
     val ticTacToe = TicTacToe()
 
-    ticTacToe.grid.size should be (0)
+    ticTacToe.grid.size should be (9)
+    ticTacToe.grid.forall(_ == null) should be (true)
   }
 
-  // "TicTacToe" should "be instantiated with an empty grid" {
-  //   val ticTacToe = TicTacToe()
-
-  //   ticTacToe.grid should be empty
-  // }
-
+  "TicTacToe" should "store a move" in {
+    val ticTacToe = TicTacToe()
+    ticTacToe.grid.forall(_ == null) should be (true)
+  }
 
 }
 
