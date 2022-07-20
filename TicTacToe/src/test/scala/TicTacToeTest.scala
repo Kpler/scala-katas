cf752dcbf1c48d98a1c02cee5acb9a7cb360fac1
cassiebrooks <cassbrooks@outlook.com>
2022-07-20 09:52:23 +0000

    superwip

--- TicTacToe/src/test/scala/TicTacToeTest.scala
+++ TicTacToe/src/test/scala/TicTacToeTest.scala
@@ -3,8 +3,15 @@ import flatspec._
 import matchers._
 
 case class TicTacToe(val grid: List[String] = List(null, null, null, null, null, null, null, null, null) ) {
-
+  def play(position: String): TicTacToe = {
+    return this
+  }
 }
+
+// index 0 = a1
+// index 1 = a2
+// index 2 = a3
+// index 3 = b4
 
 
 class TicTacToeTest extends AnyFlatSpec with should.Matchers {
@@ -17,7 +24,9 @@ class TicTacToeTest extends AnyFlatSpec with should.Matchers {
 
   "TicTacToe" should "store a move" in {
     val ticTacToe = TicTacToe()
-    ticTacToe.grid.forall(_ == null) should be (true)
+    val newTicTacToe = ticTacToe.play("a1")
+
+    newTicTacToe.grid.get(0) should be ("x")
   }
 
 }
