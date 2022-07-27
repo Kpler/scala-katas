efe5c850fa04acb1b282b57a63dc8534cee4aee2
MOTRIEUX Thomas <36075240+motrieux-thomas@users.noreply.github.com>
2022-07-27 09:45:46 +0000

    WIP

--- BowlingGame/src/test/scala/GameSpec.scala
+++ BowlingGame/src/test/scala/GameSpec.scala
@@ -17,7 +17,23 @@ class GameSpec extends AnyFlatSpec with should.Matchers {
     result should be(1)
   }
 
-  "full game" should "have 10 turns?" in {}
+  "full game" should "have 10 turns?" in {
+    val game = new Game();
+    for (i <- 0 until 20) {
+      game.roll(1);
+    }
+    val result = game.score()
+    result should be(20)
+  }
+
+  "full game" should "not have more than 10 turns?" in {
+    val game = new Game();
+    for (i <- 0 until 22) {
+      game.roll(1);
+    }
+    val result = game.score()
+    result should be(20)
+  }
 
   // TODO: manage spares, manage strikes, then manage checks below:
   // "one roll" should "should not be greater than 10" in {}
