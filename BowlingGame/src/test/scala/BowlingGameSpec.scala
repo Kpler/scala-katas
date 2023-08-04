136ba110a37993a1941a22f4e590035121371bcc
Nicolas Guignard <nicolas065@gmail.com>
2023-08-04 10:06:01 +0200

    test: first failing test

---
+++ BowlingGame/src/test/scala/BowlingGameSpec.scala
@@ -0,0 +1,27 @@
+import org.scalatest._
+import flatspec._
+import matchers._
+
+class BowlingGameSpec extends AnyFlatSpec with should.Matchers {
+
+  "An empty game" should "return a score of 0" in {
+    // Given
+    val game = new BowlingGame()
+    for (i <- 1 to 20) {
+      game.roll(0)
+    }
+    // When
+    val score = game.score()
+    // Then
+    score should be (0)
+  }
+}
+
+case class BowlingGame() {
+  def score() : Int = {
+    0
+  }
+
+  def roll(pins: Int) : Unit = ???
+
+}
