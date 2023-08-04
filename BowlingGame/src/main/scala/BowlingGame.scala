09fb143c49a5301ec68db989810f0d141a21e59b
mmetan0 <mmetan@kpler.com>
2023-08-04 17:36:12 +0800

    feat: strike condition in score

--- BowlingGame/src/main/scala/BowlingGame.scala
+++ BowlingGame/src/main/scala/BowlingGame.scala
@@ -5,14 +5,16 @@ class BowlingGame() {
     idx % 2 == 0 && idx > 0 && (rolledPins(idx - 2) + rolledPins(idx - 1) == 10)
 
   private def isStrike(idx: Int): Boolean =
-    idx % 2 == 0 && (rolledPins(idx - 2) == 10 && rolledPins(idx - 1) == 0)
+    idx % 2 != 0 && idx >= 3 && (rolledPins(idx - 3) == 10 && rolledPins(idx - 2) == 0)
 
   def score() : Int = {
     var totalScore = 0
 
     rolledPins.zipWithIndex.foreach((zippedPins) => {
       val (currPins, idx) = zippedPins
-      if (isSpare(idx)) {
+      if(isStrike(idx)) {
+        totalScore += rolledPins(idx - 1) + currPins
+      } else if (isSpare(idx)) {
         totalScore += currPins
       }
       totalScore += currPins
