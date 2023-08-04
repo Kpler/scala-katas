cc57fe781a9a4641a1902867e4699826b1c1621f
mmetan0 <mmetan@kpler.com>
2023-08-04 16:45:43 +0800

    added test with spare

--- BowlingGame/src/test/scala/BowlingGameSpec.scala
+++ BowlingGame/src/test/scala/BowlingGameSpec.scala
@@ -28,22 +28,26 @@ class BowlingGameSpec extends AnyFlatSpec with should.Matchers {
     score should be (20)
   }
 
-  "A normal game" should "return a score of 20" in {
+  "In a game with 1 spare" should "return a score of 33" in {
     // Given
     val game = new BowlingGame()
-    for (i <- 1 to 20) {
+    game.roll(5)
+    game.roll(5)
+    game.roll(3)
+    for (i <- 1 to 17) {
       game.roll(1)
     }
     // When
     val score = game.score()
     // Then
-    score should be (20)
+    score should be (33)
   }
 }
 
 class BowlingGame() {
   var rolledPins = List[Int]()
   def score() : Int = {
+
     rolledPins.sum
   }
 
