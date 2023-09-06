39932991da01ea578e020ff1528700c1429d886c
Nicolas Guignard <nicolas065@gmail.com>
2023-09-06 10:21:22 +0200

    skeleton of the first tests

--- TicTacToe/src/test/scala/Example.scala
+++ TicTacToe/src/test/scala/Example.scala
@@ -4,8 +4,25 @@ import matchers._
 
 class Example extends AnyFlatSpec with should.Matchers {
 
-  "A List" should "drop the first values" in {
-    val stack = List(3, 2)
-    stack.drop(1) should be (List(2))
+  "At the start of a game, the score" should "be love-love" in {
+    // Given
+    val game = TennisGame()
+    // When
+    val result = game.getScore()
+    // Then
+    result should be ("love-love")
   }
+
+  "When player 1 score the first point, it" should "return a score of 15-0" in {
+
+  }
+
+  "When player 2 score the first point, it" should "return a score of 0-15" in {
+
+  }
+}
+
+case class TennisGame() {
+  def getScore() : String = ???
+
 }
