cae3afe303179ad53247f58887960220b9f06d9c
Pierre Karpov <karpovpierre@gmail.com>
2023-10-26 17:28:22 +0800

    15-15

--- TicTacToe/src/test/scala/freight/TennisGameSpec.scala
+++ TicTacToe/src/test/scala/freight/TennisGameSpec.scala
@@ -1,32 +1,40 @@
 package freight
 
-import freight.TennisGame.{FIFTEEN, INITIAL, THIRTHY, THIRTY}
+import freight.TennisGame.{FIFTEEN, INITIAL, THIRTY}
 import org.scalatest.flatspec._
 import org.scalatest.matchers._
 
 class TennisGameSpec extends AnyFlatSpec with should.Matchers {
 
+
   "a starting game" should "have score set to love on both side" in {
     val game = new TennisGame()
     val score: Score = game.score()
-    score shouldBe Score("love", "love")
+    score shouldBe Score(INITIAL, INITIAL)
   }
 
   "given a starting game when player one score one point then Score" should "be (15,love)" in {
     val game = new TennisGame()
-    game.playerOneScore().score() shouldBe Score("15", "love")
+    game.playerOneScore().score() shouldBe Score(FIFTEEN, INITIAL)
   }
 
   "given a starting game when player two score one point then Score" should "be (love,15)" in {
     val game = new TennisGame()
-    game.playerTwoScore().score() shouldBe Score("love", "15")
+    game.playerTwoScore().score() shouldBe Score(INITIAL, FIFTEEN)
   }
 
-  "given a game with score 15-live, when player mark one point then score" should "be 30-love" in {
+  "given a game with score 15-love, when player mark one point then score" should "be 30-love" in {
     val game = new TennisGame()
     game
       .playerOneScore()
-      .playerOneScore().score() shouldBe Score("30", "love")
+      .playerOneScore().score() shouldBe Score(THIRTY, INITIAL)
+  }
+
+  "given a game with score 15-love, when player 2 score one point then score" should "be 15-15" in {
+    val game = new TennisGame()
+    game
+      .playerOneScore()
+      .playerTwoScore().score() shouldBe Score(FIFTEEN, FIFTEEN)
   }
 
 }
@@ -36,9 +44,6 @@ object TennisGame {
   val THIRTY = "30"
 }
 class TennisGame(val scoreInitial: Score = Score(INITIAL, INITIAL)) {
-  def playerTwoScore():  TennisGame={new TennisGame(Score(INITIAL,FIFTEEN))}
-
-
   def playerOneScore(): TennisGame = {
     scoreInitial match {
       case Score(INITIAL,INITIAL) => new TennisGame(Score(FIFTEEN, INITIAL))
@@ -46,6 +51,15 @@ class TennisGame(val scoreInitial: Score = Score(INITIAL, INITIAL)) {
         new TennisGame(Score(THIRTY, INITIAL))
     }
   }
+
+  def playerTwoScore():  TennisGame={
+    scoreInitial match {
+      case Score(INITIAL, INITIAL) => new TennisGame(Score(INITIAL,FIFTEEN))
+      case _ =>
+        new TennisGame(Score(FIFTEEN, FIFTEEN))
+    }
+  }
+
   def score(): Score = scoreInitial
 
 }
