7d3848a9c3bb0f76ee413ab4e5e9dbb34eca85ea
Germain <ggeoffroy@kpler.com>
2023-10-26 16:55:24 +0800

    wip

--- TicTacToe/src/test/scala/freight/TennisGameSpec.scala
+++ TicTacToe/src/test/scala/freight/TennisGameSpec.scala
@@ -3,16 +3,23 @@ package freight
 import org.scalatest.flatspec._
 import org.scalatest.matchers._
 
-class Example extends AnyFlatSpec with should.Matchers {
+class TennisGameSpec extends AnyFlatSpec with should.Matchers {
 
   "a starting game" should "have score set to love on both side" in {
     val game = new TennisGame()
     val score: Score = game.score()
     score shouldBe Score("love", "love")
   }
+  "given a starting game when a player score one point then Score" should "be (15,love)" in {
+    val game = new TennisGame()
+    val score = game.playerOneScore()
+    score.score() shouldBe Score("15","love")
+  }
 
 }
 class TennisGame() {
+  def playerOneScore() = new
+
   val INITIAL = "love"
   def score(): Score = {
     Score(INITIAL, INITIAL)
