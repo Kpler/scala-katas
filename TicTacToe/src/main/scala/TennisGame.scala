5f99800c8a808c7bc472370bb0385e813e753a5e
Jan Warnecke-Hasselberg <jhasselberg@kpler.com>
2023-11-15 10:44:05 +0100

    refactor: addPoint function

--- TicTacToe/src/main/scala/TennisGame.scala
+++ TicTacToe/src/main/scala/TennisGame.scala
@@ -1,7 +1,7 @@
 case class TennisGame(
                        player1: Player = Player("love"),
                        player2: Player = Player("love")) {
-  def isValid: Boolean = ???
+  def isValid: Boolean = false
 
 
   val getScore = {
@@ -12,25 +12,25 @@ case class TennisGame(
     }
   }
 
-  def addOnePointToPlayer1(): TennisGame = TennisGame(player1.addPoint(), player2)
+  def addOnePointToPlayer1(): TennisGame = TennisGame(player1.addPoint()._1, player2)
 
 
 
 
-  def addOnePointToPlayer2(): TennisGame = TennisGame(player1, player2.addPoint())
+  def addOnePointToPlayer2(): TennisGame = TennisGame(player1, player2.addPoint()._1)
 }
 
 object TennisGame {
-  val addOnePointToPlayer : TennisGame => TennisGame =  tg => tg.copy(tg.player1.addPoint(), tg.player2)
+  val addOnePointToPlayer : TennisGame => TennisGame =  tg => tg.copy(tg.player1.addPoint()._1, tg.player2)
 }
 
 
 case class Player(score:String) {
-  def addPoint(): Player = score match {
-    case "love" => Player("15")
-    case "15" => Player("30")
-    case "30" => Player("40")
-    case "40" => Player("win")
-    case "win"=> Player("win")
+  def addPoint(): (Player, Boolean) = score match {
+    case "love" => (Player("15"), true)
+    case "15" => (Player("30"), true)
+    case "30" => (Player("40"), true)
+    case "40" => (Player("win"), true)
+    case "win"=> (Player("win"), false)
   }
 }
