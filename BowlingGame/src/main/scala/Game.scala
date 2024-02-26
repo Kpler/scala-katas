1c63d69a074e9dccae661855aec4995a95af7999
manolisan <manastasiou@kpler.com>
2024-02-26 12:41:22 +0200

    score calculation try

--- BowlingGame/src/main/scala/Game.scala
+++ BowlingGame/src/main/scala/Game.scala
@@ -2,10 +2,17 @@ class Game {
   var scores: Array[Int] = Array()
 
   def score(): Int = {
-    scores.grouped(2).foldLeft(0)((acc, y) => acc + sum(y))
+    scores.grouped(2).foldLeft((0, false)) { case ((acc, isSpare), Array(x, y)) => {
+      val isSpareNext = (x + y == 10)
+      val actualXValue = if (isSpare) x + x else x
+      (acc + actualXValue + y, isSpareNext)
+    }
+    }._1
+
   }
 
-  def roll(i: Int) = {
+  def roll(i: Int): Unit = {
     scores + i
+    Unit
   }
 }
