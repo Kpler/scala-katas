2edffffe3e893e58bba42e92b19ea4105f7184fe
Simon Boeuf <sboeuf@ekinox.io>
2024-02-26 11:03:44 +0100

    failing spare test

--- BowlingGame/src/test/scala/Example.scala
+++ BowlingGame/src/test/scala/Example.scala
@@ -6,15 +6,24 @@ class Example extends AnyFlatSpec with should.Matchers {
 
   "the game" should "start" in {
     val game = new Game()
-    game.score() should be (0)
+    game.score() should be(0)
   }
 
   "the game score" should "be incremented when the player rolls" in {
     val game = new Game()
     game.roll(1)
-    game.score() should be (1)
+    game.score() should be(1)
   }
 
-  "the spare" should "be "
+  "the spare" should "increment the last roll twice if there is a spare" in {
+    val game = new Game()
+    game.roll(3)
+    game.roll(7) // frame score : 3 + 7 + 4
+
+    // Spare, next roll will be multiplied by 2
+    game.roll(4)
+
+    game.score() should be(18) // 3 + 7 + 4 + 4
+  }
 
 }
