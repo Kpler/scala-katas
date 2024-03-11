cf0fd28da8b32840148376211a4ca6ae271e9e8d
Damien GOUYETTE <dgouyette@kpler.com>
2024-03-11 10:59:32 +0100

    feat: use list of list of string to store state

--- TicTacToe/src/main/scala/Game.scala
+++ TicTacToe/src/main/scala/Game.scala
@@ -1,6 +1,6 @@
 class Game {
   val cols: List[Int] = List(1,2,3)
-  val rows: List[Int] = List(1,2,3)
+  val rows: List[List[String]] = List(List("1","2","3"), List("4","5","6"), List("7", "8", "9"))
   val playerX: Player = Player("X")
   val playerO: Player = Player("O")
     
--- TicTacToe/src/test/scala/GameSpec.scala
+++ TicTacToe/src/test/scala/GameSpec.scala
@@ -1,19 +1,13 @@
 import org.scalatest._
-import flatspec._
-import matchers._
+import org.scalatest.flatspec._
+import org.scalatest.matchers._
 
 class GameSpec extends AnyFlatSpec with should.Matchers {
 
-  "A Game" should "consist of 3 columns" in {
+  "A Game" should "have 3 rows" in {
     val game = new Game()
 
-    game.cols should have size 3
-  }
-
-  "A Game" should "have 3 rows" in {
-      val game = new Game()
-
-      game.rows should have size 3
+    game.rows should have size 3
   }
 
   "A Game" should "have playerX and playerO" in {
@@ -22,4 +16,12 @@ class GameSpec extends AnyFlatSpec with should.Matchers {
     game.playerX.name shouldEqual "X"
     game.playerO.name shouldEqual "O"
   }
+
+  "A new game" should "have its rows with index as value" in {
+
+    val game = new Game()
+    game.rows should equal(List(List("1", "2", "3"), List("4", "5", "6"), List("7", "8", "9")))
+
+  }
+
 }
