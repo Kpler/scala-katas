b6393a360bd2a66f75a0476ba0ffd508fd1263da
Nicolas Guignard <nicolas065@gmail.com>
2024-03-16 16:43:14 +0100

    feat: get started in vanilla scala

--- TicTacToe/src/main/scala/Main.scala
+++ TicTacToe/src/main/scala/Main.scala
@@ -1,5 +1,97 @@
+import scala.annotation.tailrec
+import scala.math.{ abs, max }
+
 object Main {
+
   def main(args: Array[String]): Unit = {
-    println("Hello World!")
+    println("Hello Scala FANS !!!")
+
+    // Function literals and what not: EXAMPLES
+    println("### isLessThan ###")
+    println(FirstProgram.isLessThan(2, 3))
+
+    println("### isLessThan2 ###")
+    println(FirstProgram.isLessThan2.apply(2, 3))
+
+    println("### isLessThan2 but without apply")
+    println(FirstProgram.isLessThan2(2, 3))
+
+    println("### isLessThan3 ###")
+    println(FirstProgram.isLessThan3(2, 3))
+
+    // Todo: what does the warning tell us?
+    // Todo: Dive into Function2. What does the class hierarchy tell us?
+
+    // Inner function
+    // Todo: Write a factorial function in FpSpec using recursive call. What did the compiler teach you?
+
+    // Higher order functions
+    SecondProgram.printAbsAndFactorial()
+
+    // todo: write a public format function that will take a string, the integer and a function
+    // println(FirstProgram.yourHigherOrderFunction(the string (absolute value / factorial), the integer, and the function))
+
+    println(SecondProgram.higherOrderFormat("absolute value", -42, abs))
+    println(SecondProgram.higherOrderFormat("factorial", 4, SecondProgram.factorial))
+
+    // Todo: Go make some generic function in FpSpec
+
+    // Todo: Go make isSorted tests work, tips: higher order function all the way
+
+    // function composition:
+    val sin: Double => Double = (x: Double) => Math.sin(x)
+    val transf: Double => Double = (x: Double) => (Math.PI / 2) - x
+    val fn: Double => Double = x => sin(transf(x))
+    val fnWithAndThen: Double => Double = x => (transf andThen sin)(x)
+    val fnWithCompose: Double => Double = x => (sin compose transf)(x)
+
+    println("### function composition ###")
+    println(fn(0.2d))
+    println(fnWithAndThen(0.2d))
+    println(fnWithCompose(0.2d))
+
+
   }
+}
+
+object FirstProgram {
+  val isLessThan = (x: Int, y: Int) => x < y
+  val isLessThan2 = new Function2[Int, Int, Boolean] {
+    def apply(x: Int, y: Int): Boolean = x < y
+  }
+
+  def isLessThan3(x: Int, y: Int): Boolean = x < y
+
+}
+
+object SecondProgram {
+  def factorial(n: Int): Int = {
+    @tailrec
+    def go(n: Int, acc: Int): Int = {
+      if (n <= 0)
+        acc
+      else go(n - 1, n * acc)
+    }
+    go(n, 1)
+  }
+
+  def formatAbs(x: Int): String = {
+    val msg = "The absolute value of %d is %d."
+    msg.format(x, abs(x))
+  }
+
+  def formatFactorial(n: Int): String = {
+    val msg = "The factorial of %d is %d."
+    msg.format(n, factorial(n))
+  }
+
+  def higherOrderFormat(msg: String, n: Int, fn: Int => Int): String = {
+    val newMsg = "The %s of %d is %d."
+    newMsg.format(msg, n, fn(n))
+  }
+
+  def printAbsAndFactorial(): Unit =
+    println(formatAbs(-42))
+  println(formatFactorial(7))
+
 }
---
+++ TicTacToe/src/test/scala/FpSpec.scala
@@ -0,0 +1,79 @@
+import org.scalatest._
+import flatspec._
+import matchers._
+
+class FpSpec extends AnyFlatSpec with should.Matchers {
+  private def fpFactorial(n: Int): Int = {
+    ???
+  }
+
+  "you" should "write a function able to compute the factorial of integers up to 2" in {
+    val result =  fpFactorial(2)
+    result should equal(2)
+  }
+
+  "you" should "write a function able to compute the factorial of integers up to 3" in {
+    val result =  fpFactorial(3)
+    result should equal(6)
+  }
+
+  "you" should "write a function able to compute the factorial of integers up to 4" in {
+    val result =  fpFactorial(4)
+    result should equal(24)
+  }
+
+  def findFirst(array: Array[String], key: String): Int = {
+    @annotation.tailrec
+    def loop(n: Int): Int =
+      if (n >= array.length)
+        -1
+      else if (array(n) == key)
+        n
+      else loop(n + 1)
+    loop(0)
+  }
+
+  def findFirst(array: Array[Int], key: Int): Int = {
+    @annotation.tailrec
+    def loop(n: Int): Int =
+      if (n >= array.length)
+        -1
+      else if (array(n) == key)
+        n
+      else loop(n + 1)
+    loop(0)
+  }
+
+  "findFirst" should "become a generic function called genericFindFirst and work for Char" in {
+    val chars: Array[Char] = Array('b', 'a', 'c', 'd')
+
+    // Be careful you need to change the signature to pass a function that will test equality
+    // get me the index of a
+//    genericFindFirst(chars, stuff) should be(1)
+  }
+
+  "findFirst" should "become a generic function called genericFindFirst and work for String" in {
+    val chars: Array[String] = Array("b", "a", "c", "d")
+
+    // Be careful you need to change the signature to pass a function that will test equality (but create a val function)
+    // get me the index of a
+//    genericFindFirst(chars, stuff) should be(1)
+  }
+
+  def isSorted[A](arr: Array[A], gt: (A, A) => Boolean): Boolean = {
+    ???
+  }
+
+  "isSorted" should "confirm it is when it s the case for integers" in {
+    isSorted(Array(1, 2, 3), (x: Int, y: Int) => x > y) should be(true)
+  }
+
+  "isSorted" should "return false when it s NOT the case for integers" in {
+    isSorted(Array(2, 1, 3), (x: Int, y: Int) => x > y) should be(false)
+  }
+
+
+  "isSorted" should "confirm it is when it s the case for floats" in {
+    isSorted(Array(1.3F, 2.0F, 3F), (x: Float, y: Float) => x > y) should be(true)
+  }
+}
