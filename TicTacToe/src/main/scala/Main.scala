0581111a794b3c3a98dd5f62ccdd70bf1bf86b8e
ejaoua <ejaoua@kpler.com>
2024-03-26 09:17:12 +0100

    feat(main): add toString

--- TicTacToe/src/main/scala/Main.scala
+++ TicTacToe/src/main/scala/Main.scala
@@ -1,19 +1,15 @@
 import scala.annotation.tailrec
 import scala.math.{ abs, max }
 
-class Employee(val firstName: String, val lastName: String)
+class Employee(val firstName: String, val lastName: String) {
+  override def toString : String = {
+   s"$firstName  $lastName"
+  }
+}
 
 object Main {
   def main(args: Array[String]): Unit = {
     val employee = new Employee("Daniel", "Merkel")
     println(employee)
-
-    // Todo: Implement the singly linked list of MyList in a functional way - using the variadic function syntax
-
-    // Pattern matching
-    // Todo: I want that if some variable named stuffList (for instance) of type List of Int has more than 1 element
-    // then it returns the first element otherwise it return -1
-
-    // Todo: Let's do it again with our MyList
   }
 }
