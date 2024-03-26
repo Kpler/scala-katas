d93f5ef26f996e7a72f73bfd83ac921650d3d2bf
Jan Warnecke-Hasselberg <jhasselberg@kpler.com>
2024-03-26 17:46:03 +0100

    feat: more impl

--- TicTacToe/src/main/scala/Main.scala
+++ TicTacToe/src/main/scala/Main.scala
@@ -5,9 +5,24 @@ import java.time.Instant
 
 class Employee(val firstName: String, val lastName: String) {
   var dateOfBirth: Instant = null
+  var address: String = null
   override def toString : String = {
    s"$firstName $lastName $dateOfBirth"
   }
+
+  def ageOfEmployee(): Long = {
+    (Instant.now().getEpochSecond() - dateOfBirth.getEpochSecond()) / 60 / 60 / 24 / 365
+  }
+
+  def setAdress(newAddress: String) = {
+      address = newAddress
+  }
+
+  def setAdress(newAdress: String, newDateOfBirth: Instant) = {
+      address = newAdress
+      dateOfBirth = newDateOfBirth
+  }
+
 }
 
 case class Employee2(firstName: String, lastName: String) {
@@ -19,6 +34,11 @@ object Main {
     val employee = new Employee("Daniel", "Merkel")
     val employeeTwo = Employee2("Ben", "Janowski")
     println(employee)
+    println(employee.dateOfBirth)
+    employee.dateOfBirth = Instant.parse(
+      "2001-01-01T00:00:00Z"
+    )
+    println(employee.ageOfEmployee())
     println(employeeTwo)
   }
 }
