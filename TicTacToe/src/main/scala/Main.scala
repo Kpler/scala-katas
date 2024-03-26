12cff4833402afffd43fd7ae0869cb8182456861
Jan Warnecke-Hasselberg <jhasselberg@kpler.com>
2024-03-26 10:14:54 +0100

    feat: latest changes before stand up

--- TicTacToe/src/main/scala/Main.scala
+++ TicTacToe/src/main/scala/Main.scala
@@ -1,15 +1,24 @@
 import scala.annotation.tailrec
 import scala.math.{ abs, max }
+import java.time.Instant
+
 
 class Employee(val firstName: String, val lastName: String) {
+  var dateOfBirth: Instant = null
   override def toString : String = {
-   s"$firstName  $lastName"
+   s"$firstName $lastName $dateOfBirth"
   }
+}
+
+case class Employee2(firstName: String, lastName: String) {
+
 }
 
 object Main {
   def main(args: Array[String]): Unit = {
     val employee = new Employee("Daniel", "Merkel")
+    val employeeTwo = Employee2("Ben", "Janowski")
     println(employee)
+    println(employeeTwo)
   }
 }
