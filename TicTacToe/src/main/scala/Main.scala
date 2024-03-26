eee0a85c2e3f2d48cefddb3881fd0995ab27888f
Jan Warnecke-Hasselberg <jhasselberg@kpler.com>
2024-03-26 23:05:35 +0100

    feat: add Employee2

--- TicTacToe/src/main/scala/Main.scala
+++ TicTacToe/src/main/scala/Main.scala
@@ -33,7 +33,41 @@ class Employee(val firstName: String, val lastName: String) {
 }
 
 case class Employee2(firstName: String, lastName: String) {
+  var dateOfBirth: Instant = null
+  var address: String = null
+  override def toString : String = {
+    s"$firstName $lastName $dateOfBirth"
+  }
 
+  override def equals(that: Any): Boolean = {
+    that match {
+      case that: Employee2 => this.firstName == that.firstName && this.lastName == that.lastName
+      case _ => false
+    }
+  }
+
+  override def hashCode(): Int = {
+    firstName.hashCode() + lastName.hashCode()
+  }
+
+  def ageOfEmployeeInSeconds(): Long = {
+    val now = Instant.now()
+    val age = now.getEpochSecond() - dateOfBirth.getEpochSecond()
+    age
+  }
+
+  def getName(): String = {
+    s"$firstName $lastName"
+  }
+
+  def setAdress(newAddress: String) = {
+    address = newAddress
+  }
+
+  def setAdress(newAdress: String, newDateOfBirth: Instant) = {
+    address = newAdress
+    dateOfBirth = newDateOfBirth
+  }
 }
 
 object Main {
--- TicTacToe/src/test/scala/Employee.scala
+++ TicTacToe/src/test/scala/Employee.scala
@@ -4,17 +4,31 @@ import matchers._
 
 class Example extends AnyFlatSpec with should.Matchers {
 
-  "two instances of employee" should "be have the same toString" in {
+  "two instances of Employee" should "be have the same toString" in {
     val employee1 = new Employee("Daniel", "Merkel")
     val employee2 = new Employee("Daniel", "Merkel")
     employee1.getName().equals(employee2.getName()) shouldEqual true
     employee1.getName() == employee2.getName() shouldEqual true
   }
 
-  "two instances of employee" should "be not the same instance" in {
+  "two instances of Employee" should "be not the same instance" in {
     val employee1 = new Employee("Daniel", "Merkel")
     val employee2 = new Employee("Daniel", "Merkel")
     employee1.equals(employee2) shouldEqual false
     employee1 == employee2 shouldEqual false
+  }
+
+  "two instances of Employee2" should "be have the same toString" in {
+    val employee1 = new Employee2("Daniel", "Merkel")
+    val employee2 = new Employee2("Daniel", "Merkel")
+    employee1.getName().equals(employee2.getName()) shouldEqual true
+    employee1.getName() == employee2.getName() shouldEqual true
+  }
+
+  "two instances of Employee2" should "be the same instance" in {
+    val employee1 = new Employee2("Daniel", "Merkel")
+    val employee2 = new Employee2("Daniel", "Merkel")
+    employee1.equals(employee2) shouldEqual true
+    employee1 == employee2 shouldEqual true
   }
 }
