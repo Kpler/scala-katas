5a46194e3ca58dd04f3b80fcc045895d7104e2b7
Sebastien Guillaume <sguillaume@kpler.com>
2024-06-10 14:35:31 +0200

    feat(fs2): reword parEvalMap rests

--- fs2/src/test/scala/com/kpler/scala/kata/fs2/EvalMapSpec.scala
+++ fs2/src/test/scala/com/kpler/scala/kata/fs2/EvalMapSpec.scala
@@ -3,24 +3,31 @@ package com.kpler.scala.kata.fs2
 import cats.effect.IO
 import cats.effect.unsafe.implicits.global
 import com.kpler.scala.kata.fs2.EvalMap.{ computeParallel, computeSequential }
-import fs2.Stream
 import org.scalatest.GivenWhenThen
 import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should
+
 import scala.concurrent.duration.*
 
 class EvalMapSpec extends AnyFlatSpec with GivenWhenThen with should.Matchers {
+
   def computation(i: Int) = IO.sleep(100.millis).as(i * 10)
 
-  "computeSequential" should "should compute 6 results in time" in {
-    val computes = computeSequential(computation).take(6).compile.toList.timeout(690.millis).unsafeRunSync()
+  "computeSequential" should "should compute 6 results" in {
+    val computes = computeSequential(computation).compile.toList.unsafeRunSync()
 
     computes should be(List(0, 20, 40, 60, 80, 100))
   }
 
-  "computeParallel" should "should compute 6 results in time" in {
-    val computes = computeParallel(computation).take(6).compile.toList.timeout(390.millis).unsafeRunSync()
+  "computeParallel" should "should compute the 6 results faster than computeSequential" in {
 
-    computes should be(List(0, 20, 40, 60, 80, 100))
+    val computes = IO
+      .race(
+        computeSequential(computation).compile.toList,
+        computeParallel(computation).compile.toList,
+      )
+      .unsafeRunSync()
+
+    computes should be(Right(List(0, 20, 40, 60, 80, 100)))
   }
 }
