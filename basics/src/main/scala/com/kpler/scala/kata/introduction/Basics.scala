e30686f767df043462964b96877714c56666af58
Sebastien Guillaume <sguillaume@kpler.com>
2024-07-23 18:27:21 +0200

    feat(basics): sum

---
+++ basics/src/main/scala/com/kpler/scala/kata/introduction/Basics.scala
@@ -0,0 +1,11 @@
+package com.kpler.scala.kata.introduction
+
+object Basics {
+
+  private def sum(a: Int, b: Int): Int = a + b
+
+
+  @main def myProgram(args: String*): Unit = {
+    println(sum(2, 3))
+  }
+}
